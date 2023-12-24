4fb0e944038900a196ba64a436de0eb94166ecf2
Aziz Aslan <147252797+AzizAslann@users.noreply.github.com>
2023-12-24 14:39:56 +0300

    3

---
+++ hastanerandevusistemi/Controllers/randevularım.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hastanerandevusistemi.Controllers
+{
+    public class randevularım : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
